5cfb433b7d9b4c90baa78e30935053f8d586b637
Robin Gabriel Camacho <robingabrielcamacho@gmail.com>
2025-02-02 02:12:51 -0800

    up

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const video = document.getElementById('background-video');
-    
-    video.addEventListener('ended', function() {
-        window.location.href = 'home.html';
-    });
-});
